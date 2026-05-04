d319405842a6a7e15933ae8d7356e293e59584d4
any0509 <annadevelop01@gmail.com>
2026-05-04 12:46:09 +0900

    add AllyAI

---
+++ Source/LastArtemis/Public/LA_AIController_Ally.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
+#include "LA_AIController_Ally.generated.h"
+
+
+
+UCLASS()
+class LASTARTEMIS_API ALA_AIController_Ally : public AAIController
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/LastArtemis/Public/LA_AI_Ally.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LA_BaseCharacter.h"
+#include "LA_AI_Ally.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LASTARTEMIS_API ALA_AI_Ally : public ALA_BaseCharacter
+{
+	GENERATED_BODY()
+	
+};
