30aa0fa08f6c33f08b715eca7e4788bdf6a5cee7
zxcv11595 <zxcv11595@gmail.com>
2026-05-06 10:24:48 +0900

    [26-05-06][이진영][GameStateBase, GameModeBase, GameType]
    Mission 별 게임 로직 설정

--- Source/LastArtemis/Private/LA_GameStateBase.cpp
+++ Source/LastArtemis/Private/LA_GameStateBase.cpp
@@ -1,11 +1,16 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "LA_GameStateBase.h"
 
 ALA_GameStateBase::ALA_GameStateBase()
 	:
-	CurrentGameFlowState(ELA_GameFlowState::None)
+	CurrentGameFlowState(ELA_GameFlowState::None),
+    CurrentMissionType(ELA_MissionType::None),
+    CurrentPhaseIndex(-1),
+    CurrentPhaseType(ELA_PhaseType::None),
+    CurrentKillCount(0),
+    RequiredKillCount(0)
 {
 	
 }
@@ -24,3 +29,64 @@ ELA_GameFlowState ALA_GameStateBase::GetGameFlowState() const
 	return CurrentGameFlowState;
 }
 
+//////////////////////////////
+// Mission
+//////////////////////////////
+
+void ALA_GameStateBase::SetMissionType(ELA_MissionType NewMissionType)
+{
+    CurrentMissionType = NewMissionType;
+}
+
+ELA_MissionType ALA_GameStateBase::GetMissionType() const
+{
+    return CurrentMissionType;
+}
+
+//////////////////////////////
+// Phase
+//////////////////////////////
+void ALA_GameStateBase::SetCurrentPhase(int32 NewPhaseIndex, const FLA_PhaseData& PhaseData)
+{
+    CurrentPhaseIndex = NewPhaseIndex;
+    CurrentPhaseType = PhaseData.PhaseType;
+    CurrentObjectiveText = PhaseData.ObjectiveText;
+
+    CurrentKillCount = 0;
+    RequiredKillCount = PhaseData.RequiredKillCount;
+}
+
+void ALA_GameStateBase::AddKillCount(int32 AddCount)
+{
+    CurrentKillCount += AddCount;
+}
+
+void ALA_GameStateBase::ResetKillCount()
+{
+    CurrentKillCount = 0;
+}
+
+bool ALA_GameStateBase::IsKillObjectiveCompleted() const
+{
+    return CurrentKillCount >= RequiredKillCount;
+}
+
+int32 ALA_GameStateBase::GetCurrentPhaseIndex() const
+{
+    return CurrentPhaseIndex;
+}
+
+ELA_PhaseType ALA_GameStateBase::GetCurrentPhaseType() const
+{
+    return CurrentPhaseType;
+}
+
+int32 ALA_GameStateBase::GetCurrentKillCount() const
+{
+    return CurrentKillCount;
+}
+
+int32 ALA_GameStateBase::GetRequiredKillCount() const
+{
+    return RequiredKillCount;
+}
--- Source/LastArtemis/Public/LA_GameStateBase.h
+++ Source/LastArtemis/Public/LA_GameStateBase.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -23,8 +23,54 @@ public:
 	void SetGameFlowState(ELA_GameFlowState NewState);
 	UFUNCTION(BlueprintPure, Category = "Game State")
 	ELA_GameFlowState GetGameFlowState() const;
-	
+
+    // 미션
+    UFUNCTION(BlueprintCallable, Category = "Mission")
+    void SetMissionType(ELA_MissionType NewMissionType);
+    UFUNCTION(BlueprintPure, Category = "Mission")
+    ELA_MissionType GetMissionType() const;
+
+    // Phase
+    UFUNCTION(BlueprintCallable, Category = "Phase")
+    void SetCurrentPhase(int32 NewPhaseIndex, const FLA_PhaseData& PhaseData);
+
+    UFUNCTION(BlueprintCallable, Category = "Phase")
+    void AddKillCount(int32 AddCount = 1);
+
+    UFUNCTION(BlueprintCallable, Category = "Phase")
+    void ResetKillCount();
+
+    UFUNCTION(BlueprintPure, Category = "Phase")
+    bool IsKillObjectiveCompleted() const;
+
+    UFUNCTION(BlueprintPure, Category = "Phase")
+    int32 GetCurrentPhaseIndex() const;
+
+    UFUNCTION(BlueprintPure, Category = "Phase")
+    ELA_PhaseType GetCurrentPhaseType() const;
+
+    UFUNCTION(BlueprintPure, Category = "Phase")
+    int32 GetCurrentKillCount() const;
+
+    UFUNCTION(BlueprintPure, Category = "Phase")
+    int32 GetRequiredKillCount() const;
+
 protected:
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Game State")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Game State")
 	ELA_GameFlowState CurrentGameFlowState;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mission")
+    ELA_MissionType CurrentMissionType;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Phase")
+    ELA_PhaseType CurrentPhaseType;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Phase")
+    int32 CurrentPhaseIndex;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Phase")
+    FText CurrentObjectiveText;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Phase")
+    int32 CurrentKillCount;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Phase")
+    int32 RequiredKillCount;
+
 };
