6c88c3dc2dd1f66a0327f9d1f6b8f22a8ff9d726
noobtree <tree4961@daum.net>
2026-05-06 15:47:30 +0900

    [26-05-06][차지훈][LA_PlayerCharacter] 플레이어 캐릭터 구조 수정

--- Source/LastArtemis/Character/LA_Holder.h
+++ Source/LastArtemis/Character/LA_Holder.h
@@ -6,6 +6,9 @@
 #include "UObject/Interface.h"
 #include "LA_Holder.generated.h"
 
+// 무기 기본 클래스 전방 선언
+class ALA_WeaponBase;
+
 // This class does not need to be modified.
 UINTERFACE(MinimalAPI)
 class ULA_Holder : public UInterface
@@ -24,28 +27,32 @@ class LASTARTEMIS_API ILA_Holder
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
 	/// <summary>
-	/// 임의 액터를 장착(부착)하는 함수
+	/// 임의 무기를 장착(부착)하는 함수
 	/// </summary>
-	/// <param name="HoldActor">부착되는 액터</param>
-	/// <param name="FirstPersonMesh">1인칭 시점에서 부착되는 Mesh 컴포넌트</param>
-	/// <param name="ThirdPersonMesh">3인칭 시점에서 부착되는 Mesh 컴포넌트</param>
+	/// <param name="WeaponActor">무기 액터</param>
+	/// <param name="WeaponCharacterMesh">무기를 장착한 캐릭터의 SkeletalMesh</param>
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "ILA_Holder")
-	void AttachActorMeshes(AActor* HoldActor, UMeshComponent* FirstPersonMesh, UMeshComponent* ThirdPersonMesh);
+	void ActivateWeapon(ALA_WeaponBase* WeaponActor, USkeletalMesh* WeaponCharacterMesh);
 
 	/// <summary>
-	/// 부착되어있는 액터에 맞춘 애니메이션 몽타주를 재생하는 함수
+	/// 장착되어있는 임의 무기를 해제하는 함수
 	/// </summary>
-	/// <param name="FirstPersonMontage">1인칭 시점의 애니메이션 몽타주</param>
-	/// <param name="ThirdPersonMontage">3인칭 시점의 애니메이션 몽타주</param>
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "ILA_Holder")
-	void PlayAnimMontage(UAnimMontage* FirstPersonMontage, UAnimMontage* ThirdPersonMontage);
+	void DeactivateWeapon();
 
 	/// <summary>
-	/// 부착되어있는 액터의 정보를 HUD에 업데이트 하도록 호출하는 함수
+	/// 장착된 무기에 맞춘 애니메이션 몽타주를 재생하는 함수
+	/// </summary>
+	/// <param name="Montage">재생하려는 애니메이션 몽타주</param>
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "ILA_Holder")
+	void PlayWeaponAnimMontage(UAnimMontage* Montage);
+
+	/// <summary>
+	/// 부착되어있는 무기의 정보를 HUD에 업데이트 하는 함수
 	/// </summary>
 	/// <param name="Actor"></param>
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "ILA_Holder")
-	void UpdateHUDWidgetOnActor(AActor* HoldActor);
+	void UpdateHUDWidgetOnActor(ALA_WeaponBase* HoldActor);
 
 	/// <summary>
 	/// 카메라가 바라보고 있는 지점의 좌표를 얻는 함수
@@ -53,19 +60,4 @@ public:
 	/// <returns>바라보고 있는 지점의 좌표</returns>
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "ILA_Holder")
 	FVector GetFocusLocation();
-
-	/// <summary>
-	/// 기존에 보유하고 있으면서 비활성화 되어있는 액터를 활성화하는 함수
-	/// 들고있는 액터 교체 시 사용됨
-	/// </summary>
-	/// <param name="HoldActor">활성화 시킬 액터</param>
-	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "ILA_Holder")
-	void ActivateActor(AActor* HoldActor);
-
-	/// <summary>
-	/// 기존에 보유하고 있으면서 활성화 되어있는 액터를 비활성화하는 함수
-	/// </summary>
-	/// <param name="HoldActor">비활성화 시킬 액터</param>
-	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "ILA_Holder")
-	void DeactivateActor(AActor* HoldActor);
 };
