2abb0c0a8d7c1410adc37f5d9b11d6d7c2521d4d
seonjae01 <tjswo5536@naver.com>
2026-05-13 15:48:45 +0900

    [26-05-13][이선재][LA_WeaponBase] 무기 데이터 구조를 DataAsset으로 분리

---
+++ Source/LastArtemis/Weapon/LA_WeaponData.h
@@ -0,0 +1,107 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "Curves/CurveVector.h"
+#include "Animation/AnimMontage.h"
+#include "LA_WeaponData.generated.h"
+
+UCLASS(BlueprintType)
+class LASTARTEMIS_API ULA_WeaponData : public UPrimaryDataAsset
+{
+    GENERATED_BODY()
+
+public:
+    // Visual
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Visual")
+    USkeletalMesh* WeaponMesh = nullptr;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Visual")
+    TSubclassOf<class UAnimInstance> WeaponAnimBP = nullptr;
+
+
+    // Stats
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Stats")
+    float BaseDamage = 20.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Stats")
+    float FireRate = 0.1f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Stats")
+    float MaxRange = 5000.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Stats")
+    int32 PelletCount = 1;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Stats")
+    int32 MaxMagazineSize = 30;
+
+
+    // Aim
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Aim")
+    float DefaultFOV = 120.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Aim")
+    float AimFOV = 60.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Aim")
+    float AimDistanceOffset = 50.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Aim")
+    float AimInterpSpeed = 15.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Aim")
+    FVector DefaultMeshLocation = FVector::ZeroVector;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Aim")
+    FRotator DefaultMeshRotation = FRotator::ZeroRotator;
+
+    // Spread
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Spread")
+    float DefaultSpreadAngle = 2.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Spread")
+    float MaxSpreadAngle = 8.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Spread")
+    float MinSpreadAngle = 0.5f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Spread")
+    float SpreadIncrement = 0.5f;
+
+
+    // Recoil
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Recoil")
+    UCurveVector* RecoilCurve = nullptr;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Recoil")
+    float RecoilSpeed = 20.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Recoil")
+    float RecoilResetTime = 0.5f;
+
+
+    // Sway
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Sway")
+    float MaxSwayDegree = 4.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Sway")
+    float SwayMultiplier = 1.5f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Sway")
+    float SwaySpeed = 10.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Sway")
+    float SwayReturnSpeed = 15.f;
+
+
+    // Animation
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Animation")
+    UAnimMontage* DrawMontage = nullptr;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Animation")
+    UAnimMontage* FireMontage = nullptr;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Animation")
+    UAnimMontage* ReloadMontage = nullptr;
+};
