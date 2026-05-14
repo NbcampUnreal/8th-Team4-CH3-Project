28c43c46a04f54561d142059263094075f3c66a7
sooyeon <yuiyuij@naver.com>
2026-05-14 17:10:21 +0900

    [26-05-14][심수연] 스플래시 이미지 추가/ 옵션창 제작중

---
+++ Source/LastArtemis/Weapon/LA_AnimNotify_ReloadEnd.cpp
@@ -0,0 +1,15 @@
+﻿#include "LA_AnimNotify_ReloadEnd.h"
+#include "LA_WeaponBase.h"
+
+void ULA_AnimNotify_ReloadEnd::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+    Super::Notify(MeshComp, Animation, EventReference);
+
+    if (MeshComp && MeshComp->GetOwner())
+    {
+        if (ALA_WeaponBase* Weapon = Cast<ALA_WeaponBase>(MeshComp->GetOwner()))
+        {
+            Weapon->UpdateAmmo();
+        }
+    }
+}
