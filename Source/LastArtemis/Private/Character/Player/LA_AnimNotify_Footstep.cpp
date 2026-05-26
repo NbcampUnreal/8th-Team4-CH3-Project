9a211cf8c4cb5a4635207d2e50dbfa063655f9f8
seonjae01 <tjswo5536@naver.com>
2026-05-26 17:04:16 +0900

    [26-05-26][이선재][Player] 발소리 추가

---
+++ Source/LastArtemis/Private/Character/Player/LA_AnimNotify_Footstep.cpp
@@ -0,0 +1,64 @@
+﻿#include "Character/Player/LA_AnimNotify_Footstep.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "PhysicalMaterials/PhysicalMaterial.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+ULA_AnimNotify_Footstep::ULA_AnimNotify_Footstep(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+#if WITH_EDITOR
+    bShouldFireInEditor = false;
+#endif
+}
+
+void ULA_AnimNotify_Footstep::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+    Super::Notify(MeshComp, Animation, EventReference);
+
+    if (MeshComp == nullptr || MeshComp->GetWorld() == nullptr) return;
+
+    AActor* WeaponActor = MeshComp->GetOwner();
+    if (WeaponActor == nullptr) return;
+
+    ACharacter* OwnerCharacter = Cast<ACharacter>(WeaponActor->GetOwner());
+    if (OwnerCharacter == nullptr && MeshComp->GetAttachParent())
+    {
+        OwnerCharacter = Cast<ACharacter>(MeshComp->GetAttachParent()->GetOwner());
+    }
+
+    if (OwnerCharacter == nullptr) return;
+
+    // 플레이어가 공중에 떠 있는 상태라면 발소리를 생략
+    UCharacterMovementComponent* MovementComp = OwnerCharacter->GetCharacterMovement();
+    if (MovementComp && MovementComp->IsFalling()) return;
+
+    FVector StartLocation = OwnerCharacter->GetActorLocation();
+    FVector EndLocation = StartLocation - FVector(0.f, 0.f, TraceDistance);
+
+    FCollisionQueryParams QueryParams;
+    QueryParams.AddIgnoredActor(WeaponActor);
+    QueryParams.AddIgnoredActor(OwnerCharacter);
+    QueryParams.bReturnPhysicalMaterial = true;
+
+    FHitResult HitResult;
+    bool bHit = MeshComp->GetWorld()->LineTraceSingleByChannel(HitResult, StartLocation, EndLocation, ECC_Visibility, QueryParams);
+
+    if (bHit)
+    {
+        EPhysicalSurface SurfaceType = SurfaceType_Default;
+
+        if (HitResult.PhysMaterial.IsValid())
+        {
+            SurfaceType = HitResult.PhysMaterial->SurfaceType;
+        }
+
+        if (USoundBase** SoundToPlay = Sounds.Find(SurfaceType))
+        {
+            if (*SoundToPlay)
+            {
+                UGameplayStatics::PlaySoundAtLocation(MeshComp->GetWorld(), *SoundToPlay, HitResult.ImpactPoint);
+            }
+        }
+    }
+}
---
+++ Source/LastArtemis/Public/Character/Player/LA_AnimNotify_Footstep.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "Chaos/ChaosEngineInterface.h"
+#include "LA_AnimNotify_Footstep.generated.h"
+
+UCLASS()
+class LASTARTEMIS_API ULA_AnimNotify_Footstep : public UAnimNotify
+{
+	GENERATED_BODY()
+
+public:
+    ULA_AnimNotify_Footstep(const FObjectInitializer& ObjectInitializer);
+
+    virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+
+protected:
+    // 재질별 사운드
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Sound Settings")
+    TMap<TEnumAsByte<EPhysicalSurface>, class USoundBase*> Sounds;
+
+    // 라인트레이스로 바닥을 검사할 거리
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Sound Settings")
+    float TraceDistance = 200.f;
+};
