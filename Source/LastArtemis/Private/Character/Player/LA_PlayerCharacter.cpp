46d907567d179709c0eed20aef8ec1da406dec8c
SooYeonShim <yuiyuij@naver.com>
2026-05-26 20:43:23 +0900

    동기화

--- Source/LastArtemis/Private/Character/Player/LA_PlayerCharacter.cpp
+++ Source/LastArtemis/Private/Character/Player/LA_PlayerCharacter.cpp
@@ -1216,7 +1216,7 @@ void ALA_PlayerCharacter::InventoryAction()
         if (Inventory)
         {
             Inventory->AddToViewport();
-            
+
             // 인벤토리 초기화
             Inventory->InitializeInventory(InventoryComponent);
 
